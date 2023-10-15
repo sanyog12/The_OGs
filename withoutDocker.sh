083f9f0660871b2ad22636b44ff652c745034e89
Sanyog Mane <94354794+sanyog12@users.noreply.github.com>
2023-10-15 09:59:27 -0700

    Add files via upload

---
+++ withoutDocker.sh
@@ -0,0 +1,48 @@
+# Update the local package list information and metadata from the repositories configured on your system
+
+sudo apt update
+
+
+# Install Node.js
+
+sudo apt install nodejs
+
+
+# Install npm (Node Package Manager)
+
+sudo apt install npm
+
+
+# Install the 'express' package using npm
+
+npm install express
+
+
+# Create a new directory called 'gitdir'
+
+mkdir gitdir
+
+
+# Change to the 'gitdir' directory
+
+cd gitdir
+
+
+# Clone your Git repository (Replace <repository-url> with the actual URL)
+
+git clone https://github.com/sanyog12/practiceHosting.git
+
+
+# Change to the 'practiceHosting' directory
+
+cd practiceHosting
+
+
+# Run the 'server.js' file using Node.js
+
+node server.js
+
+
+# Display a message
+
+echo "Let's get started..."
